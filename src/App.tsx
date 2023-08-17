701ef300f4b84b131d3a58063cb7b3a710d9233f
Danrllei Miranda <danmiranda14@gmail.com>
2023-08-17 19:27:24 -0300

    req 3 finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import ShoppingCart from './pages/ShoppingCart';
-import * as api from './services/api';
+// import * as api from './services/api';
 import Search from './pages/Search';
 
 function App() {
-  const [categories, setCategories] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await api.getCategories();
-      setCategories(data);
-    };
-    fetchData();
-  }, []);
+  // const [categories, setCategories] = useState([]);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const data = await api.getCategories();
+  //     setCategories(data);
+  //   };
+  //   fetchData();
+  // }, []);
   return (
     <Routes>
       <Route path="/" element={ <Search /> } />
