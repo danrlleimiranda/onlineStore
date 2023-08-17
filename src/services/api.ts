e54463222e9717bda04348a080c378c753826456
Luana Gonçalves <luana3186@gmail.com>
2023-08-17 17:28:30 -0300

    resolve requisito 1

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,20 @@
+import { Query } from '../types/queryTypes';
+
 export async function getCategories() {
   // Implemente aqui
+  const fetchCategories = await fetch('https://api.mercadolibre.com/sites/MLB/categories')
+    .catch((erro) => erro);
+  const data = fetchCategories.json();
+  return data;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(query: Query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const fetchResult = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=$QUERY}`)
+    .then((element) => element.json())
+    .then((dados) => dados)
+    .catch((erro) => erro);
+  return fetchResult;
 }
 
 export async function getProductById() {
