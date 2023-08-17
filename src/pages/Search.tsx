e99f572b369b5a35ee7d6e4dff48575d17f7d279
Danrllei Miranda <danmiranda14@gmail.com>
2023-08-17 19:13:48 -0300

    req 2 finalizado

---
+++ src/pages/Search.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Search() {
+  return (
+    <div>
+      <h1 data-testid="home-initial-message">
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </h1>
+    </div>
+  );
+}
+
+export default Search;
